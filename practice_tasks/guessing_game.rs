24e7312ebc84104dae44d19e11f6a14838859488
president-xd <lasharimohsin19@gmail.com>
2025-07-24 11:17:59 -0700

    error fix

--- practice_tasks/guessing_game.rs
+++ practice_tasks/guessing_game.rs
@@ -13,11 +13,20 @@ fn main() {
     let mut in_string: String = String::new();
     io::stdin().read_line(&mut in_string).expect("Failed to read the user from input");
 
-    if (in_string.trim() == secret_number.to_string()){
+    if (secret_number == in_string.parse().unwrap()){
+
+        // Final Message to End to the Program
         println!("The secret number is correct");
         println!("You Won the Game");
         return;
     } else {
+        if (in_string.parse().unwrap() > secret_number){
+            println!("The secret number is greater than your guess, Try Harder");
+        } else {
+            println!("The secret number is lower than your guess, Try Harder")
+        }
+
+        // Final Message to end the program
         println!("The secret number is incorrect");
         println!("You Lost the Game");
         return;
