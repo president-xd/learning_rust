702cbf469af43e79da087018e1a5943df1ed0947
president-xd <lasharimohsin19@gmail.com>
2025-07-24 13:11:58 -0700

    Improved Gussing Game Logic

--- practice_tasks/guessing_game.rs
+++ practice_tasks/guessing_game.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng; // Crate to generate random numbers
+use std::cmp::Ordering; // Crate to compare the number with respect to its order.
 
 // Cargo.toml Content
 // [package]
@@ -14,32 +15,61 @@ use rand::Rng; // Crate to generate random numbers
 fn main() {
     println!("################### Guessing Game ###################");
 
-    println!("------------------Guess a number------------------");
+    // Random Number Generation using Rand Crate
+    let s_number: u32 = rand::rng().random_range(1..101);
 
-    let secret_number = rand::rng().random_range(1..100);
+    // Looping for continuous input
+    loop {
 
-    println!("Enter your guessed number : ");
+        // Message to ask the user to input
+        println!("Enter a number that you guessed : ");
 
-    let mut in_string: String = String::new();
-    io::stdin().read_line(&mut in_string).expect("Failed to read the user from input");
+        // Variable to store the number
+        let mut number: String = String::new();
 
-    let guess: u32 = in_string.trim().parse().expect("Please enter a valid number");
+        // Taking input from the user
+        io::stdin().read_line(&mut number).expect("Failed to read the user input");
 
-    if secret_number == guess {
-        // Final Message to End to the Program
-        println!("The secret number is correct");
-        println!("You Won the Game");
-        return;
-    } else {
-        if guess > secret_number {
-            println!("The secret number is lower than your guess, Try Harder");
-        } else {
-            println!("The secret number is greater than your guess, Try Harder");
+        // Converting number (which was string) into u_guess (that is an integer type)
+        /*
+            Trim() Function: For Space Removal (White Space)
+            Parse() Function: For Parsing the value (In our case, string to integer)
+            {
+                Another thing is OK and Error Condition
+                    - Ok, which means that the input was as desired,
+                        it will just move the number into variable
+                    - Err, which means that the input was not desired,
+                        to handle the program crash, we are asking it to continue.
+            }
+        */
+        let u_guess: u32 = match number.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        /*
+            - Match to match the input as it was desired to get.
+            - cmp() function to compare the input with the reference variable (value).
+            - Ordering (use std::cmp::Ordering), to process the comparison as
+                it is lesser (to ask user to again input the number greater than recent input),
+                greater (to ask user to again input the number less than recent input)
+                or equal (to prompt user that you won the game, break the loop).
+        */
+        match u_guess.cmp(&s_number){
+            Ordering::Less => {
+                println!("Too small!");
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+            },
+            Ordering::Equal => {
+                println!("You guessed the number!");
+                println!("You Won the Game");
+                break;
+            }
         }
-
-        // Final Message to end the program
-        println!("The secret number is incorrect");
-        println!("You Lost the Game");
-        return;
     }
 }
